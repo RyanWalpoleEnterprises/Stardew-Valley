35fa156b021604327f0acf2c89ad141a55d95888
Ryan Walpole <ryan.walpole@outlook.com>
2024-04-24 13:29:03 +1000

    [v240501] - Add Code (AdminElevate)
    Add code to the AdminElevate window so that it functions as an administrative launch mode.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Startup/AdminElevate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stardew_Mod_Manager.Startup
+{
+    public partial class AdminElevate : Form
+    {
+        public AdminElevate()
+        {
+            InitializeComponent();
+            SDVDirectory.Text = Properties.Settings.Default.StardewDir.ToString();
+
+            RunUser.Style.Border = null;
+            RunUser.Style.HoverBorder = null;
+            RunUser.Style.FocusedBorder = null;
+            RunUser.Style.PressedBorder = null;
+
+            RunAdmin.Style.Border = null;
+            RunAdmin.Style.HoverBorder = null;
+            RunAdmin.Style.FocusedBorder = null;
+            RunAdmin.Style.PressedBorder = null;
+        }
+
+        private void RunAdmin_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = Application.ExecutablePath;
+            startInfo.Arguments = Properties.Settings.Default.LaunchArguments;
+            startInfo.Verb = "runas";
+            startInfo.UseShellExecute = true;
+
+            Process.Start(startInfo);
+            Application.Exit();
+        }
+
+        private void RunUser_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.LaunchAsAdmin = "FALSE";
+            Splash splash = new Splash();
+            splash.Show();
+            this.Hide();
+        }
+
+        private void SDVDirectory_TextChanged(object sender, EventArgs e)
+        {
+            SDVDirectory.Text = Properties.Settings.Default.StardewDir.ToString();
+        }
+
+        private void LearnMoreLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://rwe-labs.gitbook.io/sdvmm/getting-started/setup-and-configuration/running-with-elevated-privileges");
+        }
+    }
+}
