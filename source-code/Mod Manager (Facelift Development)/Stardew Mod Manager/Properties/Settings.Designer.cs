8b97998975acc6098213b3db545775ff1eac698f
Ryan Walpole <ryan.walpole@outlook.com>
2024-05-01 12:20:35 +1000

    [v240501] - Implement #119 - 3
    Add a new setting to store the Nexus mod update file download in memory.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Properties/Settings.Designer.cs
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Stardew_Mod_Manager.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -332,6 +332,18 @@ namespace Stardew_Mod_Manager.Properties {
             }
             set {
                 this["NexusAPIKey"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string NexusUpdateFile {
+            get {
+                return ((string)(this["NexusUpdateFile"]));
+            }
+            set {
+                this["NexusUpdateFile"] = value;
             }
         }
     }
